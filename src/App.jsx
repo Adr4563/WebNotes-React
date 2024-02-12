4d4b9daa661ceceb0c2be2edc58eb554c1879b54
unknown <afloresu.01@gmail.com>
2024-02-12 07:46:04 -0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
-import './App.css'
 import React, { useState } from 'react';
-
-const [Notes, setNotes] = React.useState([]);
-
-const crearNota = () =>{
-  const nuevaNota = document.getElementById('miTextarea').value;
-  console.log(nuevaNota);
-}
-
+import './App.css';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const crearNota = () => {
+    const newNote = document.getElementById('miTextarea').value;
+    console.log(newNote);
+    setNotes([...notes, newNote]);
+    document.getElementById('miTextarea').value = '';
+  };
+
+  const eliminarNota = (index) => {
+    const nuevasNotas = notes.filter((_, i) => i !== index);
+    setNotes(nuevasNotas);
+  };
 
   return (
-  <div>  
-    <br/>
-      <h1 >Mis Notas</h1>
-    <br/>
-      <form><textarea id = "miTextarea"  placeholder="Algo para recordar..."/></form>
-    <br/>
-      <button type='button' onClick={crearNota} >Crear</button>   
-  </div>
-
-  )
+    <>
+      <div>
+        <br />
+        <h1>Mis Notas</h1>
+        <br />
+        <form className='form1'>
+          <textarea id="miTextarea" placeholder="Algo para recordar..."></textarea>
+        </form>
+        <form>
+          <button className = "button1" type='button' onClick={crearNota}>Crear</button>
+        </form >
+        {notes.map((nota, index) => (
+          <div className='form2'>
+            <textarea id="miTextarea" key={index}>{nota}</textarea>
+            <button className="button2"  onClick={() => eliminarNota(index)}>Borrar</button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
